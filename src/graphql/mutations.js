8126f005083162091f4b7651d11ffecb63b431ee
aangeles <daniel_1234@hotmail.es>
2019-12-07 14:13:20 -0400

    Creacion de ambiente dev en AWS
    Se creo el ambiente dev en AWS en la cuenta personal de angel angeles en la zona
    de Oregon. Los servicios agregados fueron appsync, cognito y dynamodb

---
+++ src/graphql/mutations.js
@@ -0,0 +1,144 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createLocation = `mutation CreateLocation($input: CreateLocationInput!) {
+  createLocation(input: $input) {
+    id
+    name
+  }
+}
+`;
+export const updateLocation = `mutation UpdateLocation($input: UpdateLocationInput!) {
+  updateLocation(input: $input) {
+    id
+    name
+  }
+}
+`;
+export const deleteLocation = `mutation DeleteLocation($input: DeleteLocationInput!) {
+  deleteLocation(input: $input) {
+    id
+    name
+  }
+}
+`;
+export const createContact = `mutation CreateContact($input: CreateContactInput!) {
+  createContact(input: $input) {
+    id
+    name
+    phone
+  }
+}
+`;
+export const updateContact = `mutation UpdateContact($input: UpdateContactInput!) {
+  updateContact(input: $input) {
+    id
+    name
+    phone
+  }
+}
+`;
+export const deleteContact = `mutation DeleteContact($input: DeleteContactInput!) {
+  deleteContact(input: $input) {
+    id
+    name
+    phone
+  }
+}
+`;
+export const createCategory = `mutation CreateCategory($input: CreateCategoryInput!) {
+  createCategory(input: $input) {
+    id
+    name
+    description
+  }
+}
+`;
+export const updateCategory = `mutation UpdateCategory($input: UpdateCategoryInput!) {
+  updateCategory(input: $input) {
+    id
+    name
+    description
+  }
+}
+`;
+export const deleteCategory = `mutation DeleteCategory($input: DeleteCategoryInput!) {
+  deleteCategory(input: $input) {
+    id
+    name
+    description
+  }
+}
+`;
+export const createEvent = `mutation CreateEvent($input: CreateEventInput!) {
+  createEvent(input: $input) {
+    id
+    nombre
+    category {
+      id
+      name
+      description
+    }
+    date
+    description
+    location {
+      id
+      name
+    }
+    contact {
+      id
+      name
+      phone
+    }
+    createdAt
+  }
+}
+`;
+export const updateEvent = `mutation UpdateEvent($input: UpdateEventInput!) {
+  updateEvent(input: $input) {
+    id
+    nombre
+    category {
+      id
+      name
+      description
+    }
+    date
+    description
+    location {
+      id
+      name
+    }
+    contact {
+      id
+      name
+      phone
+    }
+    createdAt
+  }
+}
+`;
+export const deleteEvent = `mutation DeleteEvent($input: DeleteEventInput!) {
+  deleteEvent(input: $input) {
+    id
+    nombre
+    category {
+      id
+      name
+      description
+    }
+    date
+    description
+    location {
+      id
+      name
+    }
+    contact {
+      id
+      name
+      phone
+    }
+    createdAt
+  }
+}
+`;
