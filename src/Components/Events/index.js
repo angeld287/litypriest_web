398b93209c57c34351f8192784fbce88c2c544b2
aangeles <daniel_1234@hotmail.es>
2019-12-07 16:30:45 -0400

    Se agrego el login y los routes
    Se configuro el login de cognito, los routes y los permisos.

---
+++ src/Components/Events/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import { MDBContainer } from "mdbreact";
+
+class Events extends React.Component {
+
+  render() {
+    return (
+      <MDBContainer>
+        <h3 className="mt-5">Events</h3>
+      </MDBContainer>
+    );
+  }
+}
+
+export default Events;
