a08b60eb30a0d7cf42eba7f470c2fb6868121479
bcsolutions2 <briancorrea0311@gmail.com>
2019-12-08 21:54:55 -0400

    separando hooks de componentes

--- src/Components/Contacts/index.js
+++ src/Components/Contacts/index.js
@@ -1,51 +1,12 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { API, graphqlOperation } from 'aws-amplify';
-import { listContacts } from './../../graphql/queries';
-import { MDBContainer, MDBBox, MDBDataTable, MDBBtn } from 'mdbreact';
+import React from 'react';
+import useContacts from './useContacts';
+import { MDBContainer, MDBBox, MDBDataTable } from 'mdbreact';
 import Spinner from '../Spinner/Spinner';
 
 import './index.css';
 
 const Contacts = () => {
-	const [ contacts, setContacts ] = useState([]);
-	const [ loading, setLoading ] = useState(true);
-	const [ error, setError ] = useState(false);
-
-	useEffect(() => {
-		const fetchContacts = async () => {
-			var contactsApi = [];
-
-			try {
-				contactsApi = await API.graphql(graphqlOperation(listContacts));
-			} catch (error) {
-				setLoading(false);
-				setError(true);
-			}
-
-			var formatedContacts = [];
-			contactsApi.data.listContacts.items.forEach((contact) => {
-				formatedContacts.push({
-					name: contact.name,
-					phone: contact.phone,
-					options: (
-						<Fragment>
-							<Link to={`contacts/${contact.id}/edit`} className="btn btn-success btn-sm">
-								Editar
-							</Link>
-							<MDBBtn color="red" size="sm">
-								Borrar
-							</MDBBtn>
-						</Fragment>
-					)
-				});
-			});
-			setContacts(formatedContacts);
-			setLoading(false);
-		};
-
-		fetchContacts();
-	}, []);
+	const { loading, error, contacts } = useContacts();
 
 	const data = {
 		columns: [
