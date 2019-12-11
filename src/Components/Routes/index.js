6221afbab45cf6b191e00beef08366a495bdbb90
Angel Daniel Angeles Toribio <daniel_1234@hotmail.es>
2019-12-11 23:55:52 +0000

    Se agregaron nuevamente los cambios que se borraron con el proposito de aplicar el merge

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 
 import { Route, Switch, Redirect } from 'react-router-dom';
 
@@ -6,12 +6,20 @@ import Home from './../Home';
 import AuthComponent from './../Authentication/AuthComponent';
 import Events from '../Events/';
 import Contacts from '../Contacts';
+import Categories from '../Categories';
+import NewContact from '../Contacts/newContact';
+import EditContact from '../Contacts/editContact';
+import Locations from '../Locations'
 
 export const Routes = ({ childProps }) => (
 	<Switch>
 		<Route exact path="/" render={() => <Home />} />
 		<ProtectedRoutePriest exact path="/events" render={Events} props={childProps} />
+		<ProtectedRoutePriest exact path="/contacts/new" render={NewContact} props={childProps} />
+		<ProtectedRoutePriest exact path="/contacts/:id/edit" render={EditContact} props={childProps} />
 		<ProtectedRoutePriest exact path="/contacts" render={Contacts} props={childProps} />
+		<ProtectedRoutePriest exact path="/categories" render={Categories} props={childProps} />
+		<ProtectedRoutePriest exact path="/locations" render={Locations} props={childProps} />
 		<ProppedRoute exact path="/signin" render={AuthComponent} props={childProps} />
 	</Switch>
 );
