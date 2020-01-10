2c43dfbc7c6ac5e6fe3910b90e15dbca39e6dd01
aangeles <daniel_1234@hotmail.es>
2020-01-09 20:19:21 -0400

    se cambio la url del servicio

--- src/Components/Functions/ExportExcel.js
+++ src/Components/Functions/ExportExcel.js
@@ -4,6 +4,6 @@ const xls = require('simple-export-excel');
 var fs = require('fs');
 
 export const Excel = (name, data) => {		
-    const url = 'https://rjo7zaf6jb.execute-api.us-west-2.amazonaws.com/dev/api/inf?name='+name+'&data='+encodeURI(JSON.stringify(data));
+    const url = 'https://rjo7zaf6jb.execute-api.us-west-2.amazonaws.com/dev/exportexcel?name='+name+'&data='+encodeURI(JSON.stringify(data));
     window.open(url); 
 }
